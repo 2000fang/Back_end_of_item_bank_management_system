5d952c2d1093b78161eb5ccb63e8ac34bc127eb1
fangqianfeng <1821941394@qq.com>
2022-04-19 14:35:06 +0800

    完成更新密码接口

--- router_handler/userinfo_handler.js
+++ router_handler/userinfo_handler.js
@@ -74,7 +74,34 @@ module.exports.getUserInfo = (req, res)=>{
 
     })
 }
+
 //更新密码
 module.exports.updatepwd = (req, res)=>{
-    res.send('ok')
+  // 根据 id 查询用户的信息
+  const sql = `select * from users where user_id=?`
+  // 执行根据 id 查询用户的信息的 SQL 语句
+  db.query(sql, req.user.user_id, (err, results) => {
+    // 执行 SQL 语句失败
+    if (err) return res.cc(err)
+    // 判断结果是否存在
+    if (results.length !== 1) return res.cc('用户不存在！')
+
+    // 判断密码是否正确
+    const compareResult = bcrypt.compareSync(req.body.oldPwd, results[0].user_password)
+    if (!compareResult) return res.cc('旧密码错误！')
+
+    // 定义更新密码的 SQL 语句
+    const sql = `update users set user_password=? where user_id=?`
+    // 对新密码进行加密处理
+    const newPwd = bcrypt.hashSync(req.body.newPwd, 10)
+    // 调用 db.query() 执行 SQL 语句
+    db.query(sql, [newPwd, req.user.user_id], (err, results) => {
+      // 执行 SQL 语句失败
+      if (err) return res.cc(err)
+      // 判断影响的行数
+      if (results.affectedRows !== 1) return res.cc('更新密码失败！')
+      // 成功
+      res.cc('更新密码成功', 0)
+    })
+  })
 }
