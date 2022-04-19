0d9d56cf328a864dc7c179058a0bdbed9a834e7a
fangqianfeng <1821941394@qq.com>
2022-04-19 21:12:13 +0800

    完成添加题目&获取题目列表接口

--- router/subject.js
+++ router/subject.js
@@ -7,7 +7,7 @@ const router = express.Router()
 //导入路由处理函数模块
 const routerHandler = require('../router_handler/sub_handler.js')
 
-router.get('/subject/cates',routerHandler.getSubInfo)
+router.get('/subject',routerHandler.getSubInfo)
 router.post('/subject/addcates',routerHandler.addSubjects)
 
 module.exports = router
--- router_handler/sub_handler.js
+++ router_handler/sub_handler.js
@@ -1,10 +1,37 @@
+const { result } = require('@hapi/joi/lib/base')
+const db = require('../db/index')
 
 //获取题目列表处理函数
 module.exports.getSubInfo = (req, res)=>{
-    res.send('获取题目信息ok')
+    const sql = 'select * from subjects where course_id = ?'
+    const subInfo = req.body
+
+    db.query(sql, req.user.course_id ,(err, results)=>{
+        if(err){
+            res.cc('获取题目信息失败')
+        } 
+        res.send({
+            "status": 0,
+            "data": results,
+        })
+        
+    } )
 }
 
 //添加题目处理函数
 module.exports.addSubjects = (req, res)=>{
-    res.send('添加题目ok')
-}
+
+    const sql = 'insert into subjects (course_id, subject_type,subject_topic,subject_score,subject_answer,subject_optiona,subject_optionb,subject_optionc,subject_optiond) values (?,?,?,?,?,?,?,?,?) '
+    const subInfo = req.body
+
+    db.query(sql, [req.user.course_id ,subInfo.subject_type,subInfo.subject_topic,subInfo.subject_score,subInfo.subject_answer,subInfo.subject_optiona,subInfo.subject_optionb,subInfo.subject_optionc,subInfo.subject_optiond],(err, results)=>{
+        if(err){
+            res.cc('添加题目信息失败')
+        } 
+        res.send({
+            "status": 0,
+            "message": "添加题目成功!",
+        })
+        
+    } )
+}   
