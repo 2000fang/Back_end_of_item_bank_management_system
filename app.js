558bbd6963364caafb57d43d755aaf3725fd6c1e
fangqianfeng <1821941394@qq.com>
2022-04-30 19:22:00 +0800

    跨域问题和json解析

--- app.js
+++ app.js
@@ -5,13 +5,27 @@ const express = require('express')
 const app = express()
 const joi = require('joi')
 
-//导入cors中间件
-const cors = require("cors")
-//将cors作为中间件
-app.use(cors())
+// //导入cors中间件
+// const cors = require("cors")
+// //将cors作为中间件
+// app.use(cors())
+
+app.all('*',function (req, res, next) {
+    res.header('Access-Control-Allow-Origin','http://localhost:3001'); //当允许携带cookies此处的白名单不能写’*’
+    res.header('Access-Control-Allow-Headers','content-type,Content-Length, Authorization,Origin,Accept,X-Requested-With'); //允许的请求头
+    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT'); //允许的请求方法
+    res.header('Access-Control-Allow-Credentials',true);  //允许携带cookies
+    next();
+});
+
+
+var bodyParser = require('body-parser')
+//在路由里使用req.body即可接收到前端传过来的数据。
 
 //解析表单数据的中间件，这个中间件只能解析这种数据
 app.use(express.urlencoded({ extended: false }))
+app.use(bodyParser.json()) //app为express框架实例对象。
+
 
 //定义res.cc中间件
 app.use((req,res,next)=>{
