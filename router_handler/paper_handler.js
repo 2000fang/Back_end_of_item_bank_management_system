e4e075a758976f38dc5137c60d339b0a9ae456ae
fangqianfeng <1821941394@qq.com>
2022-05-04 13:53:21 +0800

    初步生成试卷

--- router_handler/paper_handler.js
+++ router_handler/paper_handler.js
@@ -1,7 +1,8 @@
 //导入数据库操作函数
 const db = require('../db/index')
-const docx = require("docx")
-const { Document, Packer, Paragraph, TextRun } = docx;
+const docx = require("docx");
+const { assert } = require('joi');
+const { Document, Packer, Paragraph, TextRun, PageBreak } = docx;
 
 
 //根据前端传来的试题id数组，从数据库中读取对应的试题，并且用word打印出来
@@ -14,41 +15,131 @@ module.exports.GeneratePaperById = async (req,res)=>{
     strId=strId.split(",")
     const len = strId.length
 
-    function getAlldomain(callback){
-        let subjects = []
-        for(let i=0;i<len;i++){
-            let sql=`select * from subjects where subject_id=?`
-            db.query(sql, strId[i],function(err, results){
-                subjects.push(results[0])
-                callback(subjects)
-                     
+    console.log("xxx")
+
+    // let subjects = []
+    // // //for(let i=0;i<len;i++){
+    // //     let sql=`select * from subjects where subject_id in (10,11)`
+    // //     //console.log("strid"+strId[i])
+    // //     db.query(sql, function(err, results) {
+    // //         subjects.push(results)
+    // //         console.log(subjects)
+    // //     })
+    // //}
+    
+    // let find_id_func = (sql, id) => {
+    //     return new Promise((res, rej) => {
+    //         db.query(sql, id, (err, result) => {
+    //             res(result)
+    //         })
+    //     })
+    // }
+
+    // for(let i=0;i<len;i++) {
+    //     let sql = `select * from subjects where subject_id=?`
+    //     //let list = await find_id_func(sql, strId[i])
+    //     let list = await find_id_func(sql, strId[i])
+    //     //console.log(list[0])
+    //     subjects.push(list[0])
+    // }
+    // console.log(subjects)
+    let subjects = [
+        {
+            subject_id: 10,
+            course_name: '计算机网络',
+            subject_type: '判断题',
+            subject_topic: '测试是否成功？',
+            subject_score: 2,
+            subject_answer: '否',
+            subject_optiona: '',
+            subject_optionb: '',
+            subject_optionc: '',
+            subject_optiond: '',
+            subject_chaper: '概念' 
+        },
+        {
+            subject_id: 11,
+            course_name: '计算机网络',
+            subject_type: '选择题',
+            subject_topic: '交换种类',
+            subject_score: 2,
+            subject_answer: 'ABCD',
+            subject_optiona: '电路交换',
+            subject_optionb: '报文交换',
+            subject_optionc: '虚电路交换',
+            subject_optiond: '分组交换',
+            subject_chaper: '概念' 
+        }
+    ]
+
+    let all_para = []
+    for (let i=0; i<subjects.length; i++) {
+        let Title = []
+        let title_name = new Paragraph({
+            children: [
+                new TextRun(`${i+1}. ${subjects[i].subject_topic}`),
+            ],
         })
+        let optionA = new Paragraph({
+            children: [
+                new TextRun(`(A)  ${subjects[i].subject_optiona}`),
+            ],
+        })
+        let optionB = new Paragraph({
+            children: [
+                new TextRun(`(B)  ${subjects[i].subject_optionb}`),
+            ],
+        })
+        let optionC = new Paragraph({
+            children: [
+                new TextRun(`(C)  ${subjects[i].subject_optionc}`),
+            ],
+        })
+        let optionD = new Paragraph({
+            children: [
+                new TextRun(`(D)  ${subjects[i].subject_optiond}`),
+            ],
+        })
+        let space = new Paragraph({
+            children: [
+                new TextRun(" "),
+            ],
+        })
+        all_para.push(title_name)
+        all_para.push(optionA)
+        all_para.push(optionB)
+        all_para.push(optionC)
+        all_para.push(optionD)
+        all_para.push(space)
+
+        // *add to all para
+        //all_para.push(Title)
     }
-    }
-    getAlldomain((results)=>{
-        console.log(results)
-    }) 
-      
+    let page_break = new docx.Paragraph({
+        children: [
+            new PageBreak(),
+        ],
+    })
+    // const paragraph = new docx.Paragraph({
+    //     children: [new TextRun("Amazing Heading"), new PageBreak()],
+    // });
+    
+    all_para.push(page_break)
+
+    let test_after = new Paragraph({
+        children: [
+            new TextRun(`(D)sadjhasdhaisd`),
+        ],
+    })
+    all_para.push(test_after)
+
     const doc = new Document({
         sections: [{
             properties: {},
-            children: [
-                new Paragraph({
-                    children: [
-                        new TextRun("Hello World"),
-                        new TextRun({
-                            text: "Foo Bar",
-                            bold: true,
-                        }),
-                        new TextRun({
-                            text: "\tGithub is the best",
-                            bold: true,
-                        }),
-                    ],
-                }),
-            ],
+            children: all_para,
         }],
     });
+
 
     const b64string = await Packer.toBase64String(doc);
     
